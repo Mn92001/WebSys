fa849dda9dffbb93b9b26b5e70ed7043af1cce08
rpwl <163991520+rachelpoh@users.noreply.github.com>
2024-03-20 16:28:22 +0800

    test2

---
+++ inc/nav.inc.php
@@ -0,0 +1,7 @@
+<nav>
+    <a href="#home">Home</a>
+    <a href="#about">About Us</a>
+    <a href="#services">Services</a>
+    <a href="#tools">Tools</a>
+    <a href="#contact">Contact</a>
+</nav>
