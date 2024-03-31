89718bd35208fe6a186c93098281a95121b22a19
rpwl <163991520+rachelpoh@users.noreply.github.com>
2024-04-01 01:00:27 +0800

    1apr 0100

--- pages/project_status.php
+++ pages/project_status.php
@@ -4,10 +4,15 @@
 <head>
     <title>New Projects</title>
     <?php include "../inc/head.inc.php"; ?>
-    <?php include "../inc/header.inc.php"; ?> 
     <?php include '../inc/db.php';?> 
 
     <style>
+        main {
+            margin-top: 40px;
+            position: relative;
+            min-height: 100vh;
+        }
+        
         /* Popup form hidden by default */
         .form-popup {
             display: none;
@@ -43,50 +48,53 @@ if (isset($_SESSION['error'])) {
 ?>
 
 <?php if(mysqli_num_rows($result) > 0): ?>
-    <main class="container mt-4">
-        <h2>Project Status</h2>
+    <main>
+        <section class="container">
+            <div class="container mt-4">
+                <h2>Project Status</h2>
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Expiry Date</th>
-                    <th>Status</th>
-                    <th>Report</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php while($row = $result->fetch_assoc()): ?>
-                    <tr>
-                        <td><?php echo htmlspecialchars($row['ProjectName']); ?></td>
-                        <td><?php echo htmlspecialchars($row['ProjectExpiryDate']); ?></td>
-                        <td><?php echo htmlspecialchars($row['ProjectStatus']); ?></td>
-                        <td>
-                            <?php if ($row['ClientApprovalStatus'] == 'Pending'): ?>
-                                <a href="../processes/projects/download.php?type=briefReport&id=<?php echo $row['ProjectID']; ?>" class="btn btn-link btn-sm">Download Brief Report</a>
-                            <?php elseif($row['ClientApprovalStatus'] == 'Approved'): ?>
-                                <a href="../processes/projects/download.php?type=fullReport&id=<?php echo $row['ProjectID']; ?>" class="btn btn-link link-success btn-sm">Download Full Report</a>
-                            <?php else:?>
-                                <p>-</p>   
-                            <?php endif; ?>
-                        </td>
-                        <td>
-                            <button class="btn btn-primary" onclick="openPopup('<?php echo htmlspecialchars($row['ProjectName']); ?>', '<?php echo htmlspecialchars($row['ProjectDescription']); ?>', '<?php echo htmlspecialchars($row['CoinsOffered']); ?>', '<?php echo htmlspecialchars($row['ProjectExpiryDate']); ?>', '<?php echo htmlspecialchars($row['ProjectID']); ?>', '<?php echo htmlspecialchars($row['DateOfCompletion']); ?>')">View Details</button>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Expiry Date</th>
+                            <th>Status</th>
+                            <th>Report</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while($row = $result->fetch_assoc()): ?>
+                            <tr>
+                                <td><?php echo htmlspecialchars($row['ProjectName']); ?></td>
+                                <td><?php echo htmlspecialchars($row['ProjectExpiryDate']); ?></td>
+                                <td><?php echo htmlspecialchars($row['ProjectStatus']); ?></td>
+                                <td>
+                                    <?php if ($row['ClientApprovalStatus'] == 'Pending'): ?>
+                                        <a href="../processes/projects/download.php?type=briefReport&id=<?php echo $row['ProjectID']; ?>" class="btn btn-link btn-sm">Download Brief Report</a>
+                                    <?php elseif($row['ClientApprovalStatus'] == 'Approved'): ?>
+                                        <a href="../processes/projects/download.php?type=fullReport&id=<?php echo $row['ProjectID']; ?>" class="btn btn-link link-success btn-sm">Download Full Report</a>
+                                    <?php else:?>
+                                        <p>-</p>   
+                                    <?php endif; ?>
+                                </td>
+                                <td>
+                                    <button class="btn btn-primary" onclick="openPopup('<?php echo htmlspecialchars($row['ProjectName']); ?>', '<?php echo htmlspecialchars($row['ProjectDescription']); ?>', '<?php echo htmlspecialchars($row['CoinsOffered']); ?>', '<?php echo htmlspecialchars($row['ProjectExpiryDate']); ?>', '<?php echo htmlspecialchars($row['ProjectID']); ?>', '<?php echo htmlspecialchars($row['DateOfCompletion']); ?>')">View Details</button>
 
-                            <?php if ($row['ClientApprovalStatus'] == 'Pending'): ?>
-                                <form action="../processes/process_payment_detail.php" method="post">
-                                    <input type="hidden" name="projectID" value="<?php echo $row['ProjectID']; ?>">
-                                    <input type="hidden" name="receiver" value="Pentester">
-                                    <button class="btn btn-success" type="submit">Approve</a>
-                                </form>
-                            <?php endif; ?>
-                        </td>
-                    </tr>
-                <?php endwhile; ?>
-            </tbody>
-        </table>
-        
+                                    <?php if ($row['ClientApprovalStatus'] == 'Pending'): ?>
+                                        <form action="../processes/process_payment_detail.php" method="post">
+                                            <input type="hidden" name="projectID" value="<?php echo $row['ProjectID']; ?>">
+                                            <input type="hidden" name="receiver" value="Pentester">
+                                            <button class="btn btn-success" type="submit">Approve</a>
+                                        </form>
+                                    <?php endif; ?>
+                                </td>
+                            </tr>
+                        <?php endwhile; ?>
+                    </tbody>
+                </table>
+            </div>
+        </section>   
     </main>
 <?php else: ?>
     <p>No projects found.</p>
