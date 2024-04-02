707512afb429ea71db3610926c72942a2e4e70e3
Varshak1234 <144017966+Varshak1234@users.noreply.github.com>
2024-04-02 14:28:17 +0800

    .

--- pages/about_us.php
+++ pages/about_us.php
@@ -5,7 +5,7 @@
     <title>FortifyTech - About us</title>
     <link rel="stylesheet" href="about_us.css">
     <!-- Include Bootstrap CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.1/css/bootstrap.min.css" rel="stylesheet">
     <?php include "../inc/head.inc.php"; ?>
 </head>
 
