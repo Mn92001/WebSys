7b70a3cb9c5ef6e55b6344198b9ec591d7975f80
Mn92001 <163991493+Mn92001@users.noreply.github.com>
2024-04-05 17:30:56 +0800

    admin settled

--- pages/enquiry.php
+++ pages/enquiry.php
@@ -9,6 +9,7 @@
     <?php include "../inc/session.inc.php"; ?>
     <?php include "../inc/navadmin.inc.php"; ?>
     <?php include "../processes/admin/enquiryquery.php"; ?>
+    <link rel="stylesheet" href="../assets/css/enquiry.css">
 </head> 
 
 <body>
@@ -16,27 +17,30 @@
  
 
     <?php if(mysqli_num_rows($result) > 0): ?>
-        <main class="container mt-4">
-            <h2>Enquiries</h2>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Message</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php while($row = $result->fetch_assoc()): ?>
-                        <tr>
-                            <td><?php echo htmlspecialchars($row['Name']); ?></td>
-                            <td><?php echo htmlspecialchars($row['Email']); ?></td>
-                            <td><?php echo htmlspecialchars($row['Message']); ?></td>
+        <main>
+            <div class="container mt-4">
+                <h2>Enquiries</h2>
+                <div class="table-responsive">
+                <table class="table table-bordered border-info table-hover">
+                    <thead>
+                        <tr class="table-info">
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Message</th>
                         </tr>
-                    <?php endwhile; ?>
-                </tbody>
-            </table>
-            
+                    </thead>
+                    <tbody>
+                    <?php while($row = $result->fetch_assoc()): ?>
+                            <tr>
+                                <td><?php echo htmlspecialchars($row['Name']); ?></td>
+                                <td><?php echo htmlspecialchars($row['Email']); ?></td>
+                                <td><?php echo htmlspecialchars($row['Message']); ?></td>
+                            </tr>
+                        <?php endwhile; ?>
+                    </tbody>
+                </table>
+                </div>
+            </div>
         </main>
 
     <?php else: ?>
